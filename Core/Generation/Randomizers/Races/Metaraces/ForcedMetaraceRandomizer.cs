3e902b34f31fb4eed62a5ae85251fcb04d9453cd
Karl Speer <kspeer@qualtrax.com>
2013-09-26 10:33:23 -0400

    forced metarace tests

---
+++ Core/Generation/Randomizers/Races/Metaraces/ForcedMetaraceRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NPCGen.Core.Data.Alignments;
+using NPCGen.Core.Generation.Providers.Interfaces;
+
+namespace NPCGen.Core.Generation.Randomizers.Races.Metaraces
+{
+    public abstract class ForcedMetaraceRandomizer : BaseMetaraceRandomizer
+    {
+        public ForcedMetaraceRandomizer(IPercentileResultProvider percentileResultProvider) : base(percentileResultProvider) { }
+
+        protected override Boolean RaceIsAllowed(String metarace, Alignment alignment)
+        {
+            return !String.IsNullOrEmpty(metarace);
+        }
+    }
+}
--- Tests/Generation/Randomizers/Races/Metaraces/ForcedMetaraceRandomizerTests.cs
+++ Tests/Generation/Randomizers/Races/Metaraces/ForcedMetaraceRandomizerTests.cs
@@ -1,10 +1,35 @@
-﻿using NUnit.Framework;
+﻿using System;
+using Moq;
+using NPCGen.Core.Data.Alignments;
+using NPCGen.Core.Generation.Providers.Interfaces;
+using NPCGen.Core.Generation.Randomizers.Races.Metaraces;
+using NUnit.Framework;
 
 namespace NPCGen.Tests.Generation.Randomizers.Races.Metaraces
 {
     [TestFixture]
     public class ForcedMetaraceRandomizerTests
     {
+        [Test]
+        public void NoMetaraceIsNotAllowed()
+        {
+            var mockPercentileResultProvider = new Mock<IPercentileResultProvider>();
+            mockPercentileResultProvider.SetupSequence(p => p.GetPercentileResult(It.IsAny<String>())).Returns(String.Empty).Returns("metarace");
 
+            var randomizer = new TestForcedMetaraceRandomizer(mockPercentileResultProvider.Object);
+
+            randomizer.Randomize(new Alignment(), String.Empty);
+            mockPercentileResultProvider.Verify(p => p.GetPercentileResult(It.IsAny<String>()), Times.Exactly(2));
+        }
+
+        private class TestForcedMetaraceRandomizer : ForcedMetaraceRandomizer
+        {
+            public TestForcedMetaraceRandomizer(IPercentileResultProvider percentileResultProvider) : base(percentileResultProvider) { }
+
+            protected override Boolean RaceIsAllowed(String metarace, Alignment alignment)
+            {
+                return base.RaceIsAllowed(metarace, alignment);
+            }
+        }
     }
 }
