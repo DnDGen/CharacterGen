6112376ccf81d2ac042adf21f23563742fba1139
Karl Speer <cidthecoatrack@gmail.com>
2013-09-28 15:39:36 -0400

    base races tests

---
+++ Tests/Generation/Xml/Data/Races/BaseRaces/Good/GoodDruidBaseRacesTests.cs
@@ -0,0 +1,87 @@
+﻿using NPCGen.Core.Data.Races;
+using NUnit.Framework;
+
+namespace NPCGen.Tests.Generation.Xml.Data.Races.BaseRaces.Good
+{
+    [TestFixture]
+    public class GoodDruidBaseRacesTests : PercentileTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            tableName = "GoodDruidBaseRaces";
+        }
+
+        [Test]
+        public void GoodDruidGrayElfPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.GrayElf, 1);
+        }
+
+        [Test]
+        public void GoodDruidHighElfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.HighElf, 2, 11);
+        }
+
+        [Test]
+        public void GoodDruidWildElfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.WildElf, 12, 21);
+        }
+
+        [Test]
+        public void GoodDruidWoodElfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.WoodElf, 22, 31);
+        }
+
+        [Test]
+        public void GoodDruidForestGnomePercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.ForestGnome, 32, 36);
+        }
+
+        [Test]
+        public void GoodDruidRockGnomePercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.RockGnome, 37);
+        }
+
+        [Test]
+        public void GoodDruidHalfElfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.HalfElf, 38, 46);
+        }
+
+        [Test]
+        public void GoodDruidLightfootHalflingPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.LightfootHalfling, 47);
+        }
+
+        [Test]
+        public void GoodDruidTallfellowHalflingPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.TallfellowHalfling, 48);
+        }
+
+        [Test]
+        public void GoodDruidHalfOrcPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.HalfOrc, 49);
+        }
+
+        [Test]
+        public void GoodDruidHumanPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.Human, 50, 99);
+        }
+
+        [Test]
+        public void GoodDruidEmptyPercentile()
+        {
+            AssertEmpty(100);
+        }
+    }
+}
---
+++ Tests/Generation/Xml/Data/Races/BaseRaces/Good/GoodFighterBaseRacesTests.cs
@@ -0,0 +1,87 @@
+﻿using NPCGen.Core.Data.Races;
+using NUnit.Framework;
+
+namespace NPCGen.Tests.Generation.Xml.Data.Races.BaseRaces.Good
+{
+    [TestFixture]
+    public class GoodFighterBaseRacesTests : PercentileTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            tableName = "GoodFighterBaseRaces";
+        }
+
+        [Test]
+        public void GoodFighterDeepDwarfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.DeepDwarf, 1, 3);
+        }
+
+        [Test]
+        public void GoodFighterHillDwarfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.HillDwarf, 4, 33);
+        }
+
+        [Test]
+        public void GoodFighterMountainDwarfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.MountainDwarf, 34, 41);
+        }
+
+        [Test]
+        public void GoodFighterGrayElfPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.GrayElf, 42);
+        }
+
+        [Test]
+        public void GoodFighterHighElfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.HighElf, 43, 47);
+        }
+
+        [Test]
+        public void GoodFighterRockGnomePercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.RockGnome, 48);
+        }
+
+        [Test]
+        public void GoodFighterHalfElfPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.HalfElf, 49, 50);
+        }
+
+        [Test]
+        public void GoodFighterLightfootHalflingPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.LightfootHalfling, 51);
+        }
+
+        [Test]
+        public void GoodFighterDeepHalflingPercentile()
+        {
+            AssertContentOnSingleRoll(RaceConstants.BaseRaces.DeepHalfling, 52);
+        }
+
+        [Test]
+        public void GoodFighterHalfOrcPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.HalfOrc, 53, 57);
+        }
+
+        [Test]
+        public void GoodFighterHumanPercentile()
+        {
+            AssertContentIsInRange(RaceConstants.BaseRaces.Human, 58, 97);
+        }
+
+        [Test]
+        public void GoodFighterEmptyPercentile()
+        {
+            AssertEmpty(98, 100);
+        }
+    }
+}
