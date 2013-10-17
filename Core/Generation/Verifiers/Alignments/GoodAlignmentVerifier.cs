a6bc8a42d023a356511506c414fdae2df240e3a9
Karl Speer <kspeer@qualtrax.com>
2013-10-17 08:51:04 -0400

    Tests now passing

--- Core/Generation/Verifiers/Alignments/GoodAlignmentVerifier.cs
+++ Core/Generation/Verifiers/Alignments/GoodAlignmentVerifier.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using NPCGen.Core.Data.Races;
 using NPCGen.Core.Generation.Randomizers.CharacterClasses.Interfaces;
+using NPCGen.Core.Generation.Randomizers.Races.BaseRaces;
 using NPCGen.Core.Generation.Randomizers.Races.Interfaces;
+using NPCGen.Core.Generation.Randomizers.Races.Metaraces;
 using NPCGen.Core.Generation.Verifiers.Interfaces;
 
 namespace NPCGen.Core.Generation.Verifiers.Alignments
@@ -9,17 +12,54 @@ namespace NPCGen.Core.Generation.Verifiers.Alignments
     {
         public Boolean VerifyCompatibility(IClassNameRandomizer classRandomizer)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public Boolean VerifyCompatibility(IBaseRaceRandomizer baseRaceRandomizer)
         {
-            throw new NotImplementedException();
+            if (baseRaceRandomizer is SetBaseRaceRandomizer)
+            {
+                var setBaseRaceRandomizer = baseRaceRandomizer as SetBaseRaceRandomizer;
+                return setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Bugbear
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.DerroDwarf
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Doppelganger
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Drow
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Duergar
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Gnoll
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Goblin
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Hobgoblin
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Kobold
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Lizardfolk
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.MindFlayer
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Minotaur
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Ogre
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.OgreMage
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Orc
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Tiefling
+                    && setBaseRaceRandomizer.BaseRace != RaceConstants.BaseRaces.Troglodyte;
+            }
+
+            return true;
         }
 
         public Boolean VerifyCompatibility(IMetaraceRandomizer metaraceRandomizer)
         {
-            throw new NotImplementedException();
+            if (metaraceRandomizer is NeutralMetaraceRandomizer)
+            {
+                var neutralMetaraceRandomizer = metaraceRandomizer as NeutralMetaraceRandomizer;
+                return neutralMetaraceRandomizer.AllowNoMetarace;
+            }
+            else if (metaraceRandomizer is SetMetaraceRandomizer)
+            {
+                var setMetaraceRandomizer = metaraceRandomizer as SetMetaraceRandomizer;
+                return setMetaraceRandomizer.Metarace != RaceConstants.Metaraces.HalfFiend
+                    && setMetaraceRandomizer.Metarace != RaceConstants.Metaraces.Wereboar
+                    && setMetaraceRandomizer.Metarace != RaceConstants.Metaraces.Wererat
+                    && setMetaraceRandomizer.Metarace != RaceConstants.Metaraces.Weretiger
+                    && setMetaraceRandomizer.Metarace != RaceConstants.Metaraces.Werewolf;
+            }
+
+            return true;
         }
     }
 }
