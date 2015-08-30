ba1e73cdb199806d5ca3e8818563b58bc7c8003f
Karl Speer <cidthecoatrack@gmail.com>
2015-08-30 17:23:03 -0400

    Ages, heights, and weights

---
+++ Tests/Integration/Tables/Races/BaseRaces/Ages/Intuitive/IntuitiveHalfOrcAgesTests.cs
@@ -0,0 +1,43 @@
+﻿using CharacterGen.Common.Races;
+using CharacterGen.Tables;
+using NUnit.Framework;
+using System;
+
+namespace CharacterGen.Tests.Integration.Tables.Races.BaseRaces.Ages.Intuitive
+{
+    [TestFixture]
+    public class IntuitiveHalfOrcAgesTests : AdjustmentsTests
+    {
+        protected override String tableName
+        {
+            get { return String.Format(TableNameConstants.Formattable.Adjustments.AGEGROUPRACEAges, GroupConstants.Intuitive, RaceConstants.BaseRaces.HalfOrc); }
+        }
+
+        [Test]
+        public override void CollectionNames()
+        {
+            var names = new[]
+            {
+                AdjustmentConstants.Adulthood,
+                AdjustmentConstants.Quantity,
+                AdjustmentConstants.Die,
+                AdjustmentConstants.MiddleAge,
+                AdjustmentConstants.Old,
+                AdjustmentConstants.Venerable
+            };
+
+            AssertCollectionNames(names);
+        }
+
+        [TestCase(AdjustmentConstants.Adulthood, 14)]
+        [TestCase(AdjustmentConstants.Quantity, 1)]
+        [TestCase(AdjustmentConstants.Die, 4)]
+        [TestCase(AdjustmentConstants.MiddleAge, 30)]
+        [TestCase(AdjustmentConstants.Old, 45)]
+        [TestCase(AdjustmentConstants.Venerable, 60)]
+        public override void Adjustment(String name, Int32 adjustment)
+        {
+            base.Adjustment(name, adjustment);
+        }
+    }
+}
