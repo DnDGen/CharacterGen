02ad813179760fd994a3db12c2e72077c17b7ba2
Karl Speer <cidthecoatrack@gmail.com>
2015-10-08 15:20:13 -0400

    Revert stress test
    Make stress tests cap at 15 minutes per test.  Infinite was taking way
    too long.

--- Tests/Integration/Stress/StressTests.cs
+++ Tests/Integration/Stress/StressTests.cs
@@ -45,7 +45,7 @@ namespace CharacterGen.Tests.Integration.Stress
 
         private const Int32 ConfidentIterations = 1000000;
 #if STRESS
-        private const Int32 TimeLimitInSeconds = Int32.MaxValue;
+        private const Int32 TimeLimitInSeconds = 1000 * 60 * 15;
 #else
         private const Int32 TimeLimitInSeconds = 1;
 #endif
